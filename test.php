2bd68cfb638d63a484f6c2e92517268a01272da8
bigjim1971 <bigjim1971@users.noreply.github.com>
2015-06-01 19:19:45 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php 
+session_start();
+
+$r = ""; //user name
+$q = "";  //password
+$q = strtolower($q);
+$r = strtolower($r);
+
+echo $q . "        = ";
+$q = md5($q);
+echo $q;
+echo " " . $r . "        = ";
+$r = md5($r);
+echo $r;
+
+?> 
