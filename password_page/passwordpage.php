a0476aefcb7679806ce4cb9b04ee4c825f9b338d
bigjim1971 <bigjim1971@users.noreply.github.com>
2015-06-08 02:59:25 +0100

    Update passwordpage.php

--- password_page/passwordpage.php
+++ password_page/passwordpage.php
@@ -1,1 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
 
+<style>
+body{background-color: cyan; width: 100%; height: 100%;}
+div {margin: 200px auto ; font-size: 450%; color: black; width: 200px; height: 150px;}
+</style>
+
+</head>
+
+<body>
+
+<?php
+if (isset($_POST["user"]) && isset($_POST["pass"])) {
+	$username = $_POST["user"]; // User Name
+	$password = $_POST["pass"]; // Password
+	if (    ($username == "markey123456789@gmail.com" && $password == "ebel1")
+		 || ($username == "thomasjebel@gmail.com" && $password == "ebel2")
+	) {
+		echo("<div>Hi</div>");
+	} else {
+		echo("<div>Sorry You Are Not Allowed IN</div>");
+	} 
+} else {
+echo('
+<form action="passwordpage.php" method="post">
+Username:</br> 
+<input type="text" name="user">
+</br>
+Password:</br> 
+<input type="text" name="pass">
+</br>
+</br>
+<input type="submit" value="Log In">
+</form>
+</br></br>
+');
+}
+?>
+
+</body>
+</html>
