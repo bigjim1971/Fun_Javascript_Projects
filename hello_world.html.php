d6698b8895188cc7a15710d8b1b6ade5ae505db9
bigjim1971 <bigjim1971@users.noreply.github.com>
2015-06-08 03:10:46 +0100

    Create hello_world.html.php

---
+++ hello_world.html.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<style>
+body{ width: 100%; height: 100%; background-color: green;}
+div { width: 150px; height: 50px; background-color: green; color: black;}
+</style>
+
+</head>
+
+<body>
+
+<div id="output"> <?php echo("Hello World"); ?> </div>
+
+
+
+</body>
+</html>
