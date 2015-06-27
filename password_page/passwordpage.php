cb0c126a59781ca6ef20b178a41eea62498899c5
bigjim1971 <bigjim1971@users.noreply.github.com>
2015-06-27 20:26:56 +0100

    Update passwordpage.php

--- password_page/passwordpage.php
+++ password_page/passwordpage.php
@@ -15,8 +15,10 @@ div {margin: 200px auto ; font-size: 450%; color: black; width: 200px; height: 1
 if (isset($_POST["user"]) && isset($_POST["pass"])) {
 	$username = $_POST["user"]; // User Name
 	$password = $_POST["pass"]; // Password
-	if (    ($username == "markey123456789@gmail.com" && $password == "ebel1")
-		 || ($username == "thomasjebel@gmail.com" && $password == "ebel2")
+	$username = md5($username);
+	$password = md5($password);
+	if (    ($username == "25699f04d3a65f923ad0f2fbbfa1cb39 " && $password == "06ba39c3635ecc77bd10918c0622df11 ")
+	     || ($username == "2ac15c4f5d19aa101756f1890e3b30e0 " && $password == "7703646921faf4d03459d10c6ac82a3a ")
 	) {
 		echo("<div>Hi</div>");
 	} else {
