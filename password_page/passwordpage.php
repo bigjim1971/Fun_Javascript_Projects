7b126ae0413791d892c5a7e424f0a66a1855567f
bigjim1971 <bigjim1971@users.noreply.github.com>
2015-06-27 20:52:22 +0100

    Update passwordpage.php

--- password_page/passwordpage.php
+++ password_page/passwordpage.php
@@ -19,8 +19,8 @@ if (isset($_POST["user"]) && isset($_POST["pass"])) {
 	$password = strtolower($password);
 	$username = md5($username);
 	$password = md5($password);
-	if (    ($username == "25699f04d3a65f923ad0f2fbbfa1cb39 " && $password == "06ba39c3635ecc77bd10918c0622df11 ")
-	     || ($username == "2ac15c4f5d19aa101756f1890e3b30e0 " && $password == "7703646921faf4d03459d10c6ac82a3a ")
+	if (    ($username == "25699f04d3a65f923ad0f2fbbfa1cb39" && $password == "06ba39c3635ecc77bd10918c0622df11")
+	     || ($username == "2ac15c4f5d19aa101756f1890e3b30e0" && $password == "7703646921faf4d03459d10c6ac82a3a")
 	) {
 		echo("<div>Hi</div>");
 	} else {
