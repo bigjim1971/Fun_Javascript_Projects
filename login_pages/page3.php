08fe7f407ebb5b0359e8bd6b1399025f50a3951d
bigjim1971 <bigjim1971@users.noreply.github.com>
2015-07-05 18:07:06 +0100

    Create page3.php

---
+++ login_pages/page3.php
@@ -0,0 +1,31 @@
+<?php
+	session_start();
+	
+	
+	if (isset($_SESSION['password']) && ($_SESSION['password'] == "allowed")) {} else {
+		//header( 'Location: http://localhost/sites/login_pages/index.php' ) ;
+		header( 'Location: http://localhost:8888/sites/login_pages/index.php' ) ;
+	} 
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<style>
+body{background-color: cyan; width: 100%; height: 100%;}
+div {margin: 200px auto ; font-size: 450%; color: black; width: 200px; height: 150px;}
+</style>
+
+</head>
+
+<body>
+<form action="index.php" method="post">
+<input type="submit" value="logout" name="logout">
+</form>
+<div>Hi this is page 3</div>
+
+</body>
+</html>
