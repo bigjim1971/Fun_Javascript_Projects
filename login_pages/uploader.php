b4c188662a2620ac31b49b501d2e985b7987a520
bigjim1971 <bigjim1971@users.noreply.github.com>
2015-07-12 19:31:47 +0100

    Update uploader.php

--- login_pages/uploader.php
+++ login_pages/uploader.php
@@ -4,4 +4,18 @@ ini_set("display_errors", 1);
 
 print_r($_FILES);
 
+//What folder to save it in
+$targetPath = "uploads/";
+
+//Build the store path
+$targetPath = $targetPath . basename( $_FILES['uploadedfile']['name'] );
+
+if ( move_uploaded_file( $_FILES['uploadedfile']['tmp_name'], $targetPath) ) {
+	echo( "The file" . basename( $_FILES['uploadedfile']['name'] ) . " has been uploaded" );
+} else {
+	echo( "There was an error uploading the file, please try again!" );
+}
+
+
+
 ?>
