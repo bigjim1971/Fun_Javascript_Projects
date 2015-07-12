8fc566d839787d444eaeb4d50fbd041345616efa
bigjim1971 <bigjim1971@users.noreply.github.com>
2015-07-12 19:15:59 +0100

    Update page1.php

--- login_pages/page1.php
+++ login_pages/page1.php
@@ -3,8 +3,8 @@
 	
 	
 	if (isset($_SESSION['password']) && ($_SESSION['password'] == "allowed")) {} else {
-		//header( 'Location: http://localhost/sites/login_pages/index.php' ) ;
-		header( 'Location: http://localhost:8888/sites/login_pages/index.php' ) ;
+		header( 'Location: http://localhost/sites/login_pages/index.php' ) ;
+		//header( 'Location: http://localhost:8888/sites/login_pages/index.php' ) ;
 	} 
 ?>
 
@@ -24,6 +24,14 @@ div {margin: 200px auto ; font-size: 450%; color: black; width: 200px; height: 1
 <body>
 <form action="index.php" method="post">
 <input type="submit" value="logout" name="logout">
+</form>
+
+</br></br>
+
+<form enctype="multipart/form-data" action="uploader.php" method="POST">
+	<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+	Choose a file to upload: <input name="uploadfile" type="file" /></br>
+	<input type="submit" value="Upload File" />
 </form>
 <div>Hi this is page 1</div>
 
