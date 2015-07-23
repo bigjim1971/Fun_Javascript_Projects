46863636d5fc2e233d702fe7b5e5a5cc51bf4888
bigjim1971 <bigjim1971@users.noreply.github.com>
2015-07-23 18:52:31 +0100

    Create page3.php

---
+++ login_pages_2/page3.php
@@ -0,0 +1,28 @@
+<?php
+	require_once('load.php');
+	$loginObj->redirect()
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<style>
+body{background-color: cyan; width: 100%; height: 100%;}
+div {margin: 200px auto ; font-size: 450%; color: black; width: 200px; height: 150px;}
+</style>
+
+</head>
+
+<body>
+
+<?php
+	$loginObj->logoutButton();
+?>
+
+<div>Hi this is page 3</div>
+
+</body>
+</html>
