93a1fdfef4185004ed52436dea1de07387a6de9a
bigjim1971 <bigjim1971@users.noreply.github.com>
2015-07-23 18:50:18 +0100

    Create config.php

---
+++ login_pages_2/config.php
@@ -0,0 +1,25 @@
+<?php
+/* Confiuration Information
+ * Enter your configuration information below.
+ */
+
+// Site Info
+
+/* Local Host Path
+ * Enter the path of the local host here.
+ */
+define('localHost_path', 'http://localhost:8888/');
+//define('LocalHost_path', 'http://localhost/');
+
+/* Site Sub Path
+ * Enter the sites subdirectory here.
+ */
+define('siteSub_path', 'sites/login_pages_2/');
+
+// start Seesion
+
+/* Session Start for all pages
+ * Here we start the session once and only once.
+ */
+session_start(); 
+?>
